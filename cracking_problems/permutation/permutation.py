af4eb8a8c0d50fc8359b2295ed3d017e4966eb45
Vitaly Kruglikov <vitaly@Numentas-MacBook-Pro.local>
2017-11-22 13:49:38 -0800

    Implemented permNoAccumulator that permutes without an explicit accumulator

--- cracking_problems/permutation/permutation.py
+++ cracking_problems/permutation/permutation.py
@@ -1,5 +1,6 @@
 def permutations(values):
     """Create generator that yields permutations of the given sequence
+    using an accumulator
 
     :param values: sequence of values to permute over
     :return: generator that yields permutations of the given sequence
@@ -17,3 +18,21 @@ def _doPermutations(values, perm):
                     values=values[0:i] + values[i+1:],
                     perm=perm + values[i:i+1]):
                  yield result
+
+
+def permNoAccumulator(values):
+    """Create generator that yields permutations of the given sequence
+    without an accumulator
+
+    :param values: sequence of values to permute over
+    :return: generator that yields permutations of the given sequence
+    """
+    if not values:
+        # Passed empty sequence
+        return
+    elif len(values) == 1:
+        yield values
+    else:
+        for i in xrange(len(values)):
+            for perm in permNoAccumulator(values=values[0:i] + values[i+1:]):
+                 yield values[i:i+1] + perm
