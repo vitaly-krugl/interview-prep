2676b001dcecc474498748d4abca9d961fb5b00b
Vitaly Kruglikov <vitaly@Numentas-MacBook-Pro.local>
2017-12-11 14:37:52 -0800

    Added version of fib with memoization decorator.

--- cracking_problems/fibonacci/fib.py
+++ cracking_problems/fibonacci/fib.py
@@ -1,5 +1,5 @@
 def fib(n):
-    """Compute the Nth Fibonacci number
+    """Compute the Nth Fibonacci number using recursion without memoization
 
     :param n: the value of N for computing the Nth Fibonacci number
     :return: Nth Fibonacci number
@@ -12,6 +12,45 @@ def fib(n):
         return 1
     else:
         return fib(n - 1) + fib(n - 2)
+
+
+def _memoizeFib(func):
+    """Decorator for memoization of fib
+
+    :param func:
+    :return:
+    """
+    results = dict()
+
+    def fib_wrapper(n):
+        try:
+            value = results[n]
+        except KeyError:
+            value = func(n)
+            results[n] = value
+
+        return value
+
+    return fib_wrapper
+
+
+@_memoizeFib
+def memFib(n):
+    """Compute the Nth Fibonacci number using recursion with memoization.
+
+    NOTE We have two versions (memoized vs not) for performance comparison.
+
+    :param n: the value of N for computing the Nth Fibonacci number
+    :return: Nth Fibonacci number
+    """
+    assert n >= 0
+
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return memFib(n - 1) + memFib(n - 2)
 
 
 def fibs(n):
